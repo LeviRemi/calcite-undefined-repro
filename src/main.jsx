0ad5108277cf84801efcbd9d251b5b0aaaa4dfb1
leviremi <lremington@esri.com>
2023-12-12 13:26:47 -0800

    Update BrowserRouter basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-import './index.css'
+import "./index.css";
 import "@esri/calcite-components/dist/calcite/calcite.css";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <BrowserRouter>
-            <App />
-      </BrowserRouter>
-  </React.StrictMode>,
-)
+    <BrowserRouter basename="/calcite-undefined-repro">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
